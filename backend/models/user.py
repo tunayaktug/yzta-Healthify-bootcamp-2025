de8864c897aaaead9def1d73be8298b8ec5849a1
tunayaktug <aktugtunay87@gmail.com>
2025-07-01 09:58:20 +0300

    manually migrate files from backend-dev branch

--- backend/models/user.py
+++
@@ -1,9 +0,0 @@
-from sqlalchemy import Column, Integer, String
-from backend.database import Base
-
-class User(Base):
-    __tablename__ = "users"
-
-    id = Column(Integer, primary_key=True, index=True)
-    email = Column(String(255), unique=True, index=True)
-    password = Column(String(255))
