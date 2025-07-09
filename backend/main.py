7152e511ccc7a6362326c0955b8546aac87c298a
tunayaktug <aktugtunay87@gmail.com>
2025-07-09 14:33:53 +0300

    Revert "add user profile management and enhance chatbot UI"
    This reverts commit 1761f789d3398aba2ec05201c9ef75fa0bf838e3.

--- backend/main.py
+++ backend/main.py
@@ -1,20 +1,10 @@
-
-from dotenv import load_dotenv
-load_dotenv()  
-
 from fastapi import FastAPI
 from routers import auth, predict, news
 from database import Base, engine
 from models import user, predictor
 from fastapi.middleware.cors import CORSMiddleware
-from routers.chatbot import router as chatbot_router
-from fastapi.staticfiles import StaticFiles
-from models.user import User
-from pathlib import Path
 
-
-
-app = FastAPI(title="Healthify API")
+app = FastAPI()
 
 # CORS ayarları
 app.add_middleware(
@@ -28,22 +18,5 @@ app.add_middleware(
 app.include_router(auth.router)
 app.include_router(predict.router)
 app.include_router(news.router)
-app.include_router(chatbot_router)
-
-# 1) Bu dosyanın (main.py) bulunduğu klasör
-HERE = Path(__file__).resolve().parent
-
-# 2) Projenin kök klasörü (örneğin main.py 'backend' içinde ise bir üst klasöre çık)
-PROJECT_ROOT = HERE.parent
-
-# 3) frontend dizini
-FRONTEND_DIR = PROJECT_ROOT / "frontend"
-
-# Statik frontend dosyalarını serve et
-app.mount(
-    "/",
-    StaticFiles(directory=str(FRONTEND_DIR), html=True),
-    name="frontend"
-)
 
 Base.metadata.create_all(bind=engine)
