2156610b73e80bca0f6012fbfd941fd70c015a77
Büşra Yavuz <162350445+0busrayavuz@users.noreply.github.com>
2025-08-02 23:10:02 +0300

    Update eye_predictor.py

--- backend/models/eye_predictor.py
+++ backend/models/eye_predictor.py
@@ -1,10 +1,24 @@
-from PIL import Image
+from tensorflow.keras.models import load_model
+import numpy as np
 
 class EyePredictor:
-    def __init__(self, model_path="models/eye_model.onnx"):
-        self.model_path = model_path
-        # Model henüz mevcut değil, simülasyon kullanıyoruz
-    
-    def predict(self, image: Image):
-        """Göz tahmini yap (şimdilik simülasyon)"""
-        return "Göz çevresinde kuruluk ve kızarıklık tespit edildi." 
+    def __init__(self, model_path="models/vgg16_1.h5"):
+        self.model = load_model(model_path)
+
+    def predict(self, image):
+        img = image.resize((224, 224))
+        img = np.array(img) / 255.0
+        img = np.expand_dims(img, axis=0)
+        predictions = self.model.predict(img)
+        classes = [
+            "Normal",
+            "Diabetes",
+            "Glaucoma",
+            "Cataract",
+            "Age related Macular Degeneration",
+            "Hypertension",
+            "Pathological Myopia",
+            "Other diseases/abnormalities"
+        ]
+        pred_index = np.argmax(predictions)
+        return classes[pred_index]
