91d57a063e31fb2e1413e581fd9046836886d7c6
Büşra Yavuz <162350445+0busrayavuz@users.noreply.github.com>
2025-08-02 23:09:43 +0300

    Update hair_predictor.py

--- backend/models/hair_predictor.py
+++ backend/models/hair_predictor.py
@@ -1,10 +1,29 @@
-from PIL import Image
+from tensorflow.keras.models import load_model
+from tensorflow.keras.preprocessing import image
+import numpy as np
 
 class HairPredictor:
-    def __init__(self, model_path="models/hair_model.onnx"):
-        self.model_path = model_path
-        # Model henüz mevcut değil, simülasyon kullanıyoruz
-    
-    def predict(self, image: Image):
-        """Saç tahmini yap (şimdilik simülasyon)"""
-        return "Yoğun dökülme ve seyrelme tespit edildi." 
+    def __init__(self):
+        # Burada h5 dosyasını yüklüyoruz
+        self.model = load_model("models/VGG16-Final.h5")
+        self.class_names = [
+            'Alopecia Areata',
+            'Contact Dermatitis',
+            'Folliculitis',
+            'Head Lice',
+            'Lichen Planus',
+            'Male Pattern Baldness',
+            'Psoriasis',
+            'Seborrheic Dermatitis',
+            'Telogen Effluvium',
+            'Tinea Capitis'
+        ]
+
+    def predict(self, img):
+        img = img.resize((224, 224))
+        img_array = image.img_to_array(img)
+        img_array = np.expand_dims(img_array, axis=0)
+        img_array = img_array / 255.0
+        prediction = self.model.predict(img_array)
+        predicted_class = np.argmax(prediction, axis=1)[0]
+        return self.class_names[predicted_class]
